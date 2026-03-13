dafd8fa9cfee9b9cf998ec487f5af08cbe0106a1
Skillmio <skillmiocfs@gmail.com>
2026-03-14 00:09:59 +0100

    Update graph_banned_ips.py

--- scripts/graph_banned_ips.py
+++ scripts/graph_banned_ips.py
@@ -35,8 +35,8 @@ GRAPH_TOTAL = os.path.join(EXPORT_DIR, "banned_ips_graph.png")
 GRAPH_GROWTH = os.path.join(EXPORT_DIR, "banned_ips_growth_graph.png")
 
 BRANCH = "main"
-BOT_USER = "bot-updater"
-BOT_EMAIL = "bot@skillmio.net"
+BOT_USER = "Skillmio"
+BOT_EMAIL = "skillmiocfs@gmail.com"
 COMMIT_MSG = "Update banned IP statistics"
 
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
